bf9e79d97a51ea0bf76abac02379e2eecd762719
Christoph Herzog <chris@theduke.at>
2021-11-07 08:41:59 +0100

    ui: Small settings page styling

--- crates/ui/src/components/settings.rs
+++ crates/ui/src/components/settings.rs
@@ -1,11 +1,14 @@
 use brass::dom::{builder::div, TagBuilder};
 use semantic_ui_core::{
-    components::util::title_2,
+    components::util::{title_2, Cls},
     routing::{link, Route},
 };
 
 pub fn settings_page() -> TagBuilder {
-    div()
-        .and(title_2().and("Settings"))
-        .and(link(Route::PluginManager, "Plugins"))
+    div().and(title_2().and("Settings")).and(
+        div()
+            .style_raw("display: flex; flex-direction: column; gap: 1rem;")
+            .and(div().and(link(Route::PluginManager, "Plugins").class(Cls::Button)))
+            .and(div().and(link(Route::Tags, "Tags").class(Cls::Button))),
+    )
 }
