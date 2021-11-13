7f1f17989e303556c6c4391d34a4e2015d03eef7
Christoph Herzog <chris@theduke.at>
2021-11-13 12:50:50 +0100

    fix(ui): Small navbar styling adjustment

--- crates/ui/src/components/root.rs
+++ crates/ui/src/components/root.rs
@@ -89,6 +89,6 @@ fn navbar() -> TagBuilder {
 
     div()
         .class("navbar")
-        .style_raw("border-bottom: 2px solid black; border-radius: 10px; margin-bottom: 1rem;")
+        .style_raw("height: 52px; border-bottom: 2px solid black; border-radius: 10px; margin-bottom: 1rem;")
         .and((brand, menu))
 }
