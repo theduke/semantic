c3f1adc461c4ecdeb7654fe523cfa0b1813b8e8f
Christoph Herzog <chris@theduke.at>
2021-11-15 14:37:26 +0100

    feat(server): Optionally embed mozjpeg / oxipng
    Adds a new feature "media-optimizers" that will embed oxipng and mozjpeg
    instead of calling out to the respective binaries.

--- crates/semantic/src/util/media.rs
+++ crates/semantic/src/util/media.rs
@@ -11,13 +11,22 @@ pub fn optimize_image_data(data: &[u8]) -> Result<Vec<u8>, AnyError> {
     match kind.mime_type() {
         "image/jpeg" => {
             // Try to use jpegtran binary
-            jpegtran(data)
+            mozjpeg_jpegtran(data)
         }
         "image/png" => oxipng(data),
         other => Err(anyhow::anyhow!("Unsupported mime type: {}", other)),
     }
 }
 
+/// Losslessly optimize pngs with oxipng as an embedded library.
+#[cfg(feature = "media-optimizers")]
+fn oxipng(data: &[u8]) -> Result<Vec<u8>, AnyError> {
+    oxipng::optimize_from_memory(data, &oxipng::Options::from_preset(4))
+        .context("Could not optimize png with oxipng")
+}
+
+/// Losslessly optimize pngs with oxipng as a CLI tool.
+#[cfg(not(feature = "media-optimizers"))]
 fn oxipng(data: &[u8]) -> Result<Vec<u8>, AnyError> {
     let mut child = Command::new("oxipng")
         .stdin(Stdio::piped())
@@ -58,8 +67,55 @@ fn oxipng(data: &[u8]) -> Result<Vec<u8>, AnyError> {
     Ok(output)
 }
 
-fn jpegtran(data: &[u8]) -> Result<Vec<u8>, AnyError> {
+// /// Losslessly optimize jpegs with mozjpeg as a CLI tool.
+// #[cfg(feature = "media-optimizers")]
+// fn mozjpeg_jpegtran(data: &[u8]) -> Result<Vec<u8>, anyhow::Error> {
+//     std::panic::catch_unwind(|| -> Result<Vec<u8>, anyhow::Error> {
+//         let decomp = mozjpeg::Decompress::new_mem(data)?;
+
+//         let mut compress = mozjpeg::Compress::new(decomp.color_space());
+//         compress.set_raw_data_in(true);
+//         compress.set_size(decomp.width(), decomp.height());
+//         compress.set_optimize_scans(true);
+//         compress.set_optimize_coding(true);
+//         compress.set_mem_dest();
+
+//         let mut bitmaps: Vec<_> = decomp
+//             .components()
+//             .iter()
+//             .map(|_c| Vec::with_capacity(decomp.width() * decomp.height()))
+//             .collect();
+
+//         let mut decomp = decomp.raw()?;
+//         {
+//             let mut bitmap_refs: Vec<_> = bitmaps.iter_mut().collect();
+//             decomp.read_raw_data(&mut bitmap_refs);
+//             if !decomp.finish_decompress() {
+//                 bail!("Could not decompress image")
+//             }
+//         }
+
+//         let bitmap_refs: Vec<_> = bitmaps.iter().map(|v| v.as_slice()).collect();
+//         if !compress.write_raw_data(&bitmap_refs) {
+//             bail!("Could not compress image");
+//         }
+
+//         compress.finish_compress();
+
+//         let data = compress
+//             .data_to_vec()
+//             .map_err(|_| anyhow!("Could not compress image"))?;
+
+//         Ok(data)
+//     })
+//     .map_err(|_err| anyhow!("mozjpeg failed"))?
+// }
+
+/// Losslessly optimize jpegs with mozjpeg as a CLI tool.
+// #[cfg(not(feature = "media-optimizers"))]
+fn mozjpeg_jpegtran(data: &[u8]) -> Result<Vec<u8>, AnyError> {
     let mut child = Command::new("jpegtran")
+        .arg("-optimize")
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
         .stderr(Stdio::piped())
