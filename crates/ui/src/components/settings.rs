733aaf8cad487c41372ff388f44c9a6fb2152236
Christoph Herzog <chris@theduke.at>
2022-03-19 16:03:45 +0100

    refactor(ui): Use view! macro for backend stats

--- crates/ui/src/components/settings.rs
+++ crates/ui/src/components/settings.rs
@@ -1,8 +1,11 @@
-use brass::dom::{builder::div, TagBuilder};
+use brass::{
+    dom::{builder::div, TagBuilder},
+    view,
+};
 use semantic_ui_core::{
     components::{
         loader::load,
-        util::{box_, subtitle_4, title_2, Cls},
+        util::{title_2, Cls},
     },
     context,
     routing::{link, Route},
@@ -25,32 +28,40 @@ fn server_status() -> TagBuilder {
 
     load(f, |status| {
         let backend = status.backend_status.as_ref().map(|s| {
-            div().and(
-                div()
-                    .and(div().and(format!(
-                        "DB size: {}",
-                        s.db_size
+            let db_size = s
+                .db_size
+                .map(|x| bytesize::ByteSize(x).to_string())
+                .unwrap_or_else(|| "n/a".to_string());
+            let asset_size = s.asset_size
                             .map(|x| bytesize::ByteSize(x).to_string())
-                            .unwrap_or_else(|| "n/a".to_string())
-                    )))
-                    .and(div().and(format!(
-                        "Asset size: {}",
-                        s.asset_size
+                            .unwrap_or_else(|| "n/a".to_string());
+            let storage_size = s.storage_size
                             .map(|x| bytesize::ByteSize(x).to_string())
-                            .unwrap_or_else(|| "n/a".to_string())
-                    )))
-                    .and(div().and(format!(
-                        "Storage size: {}",
-                        s.storage_size
-                            .map(|x| bytesize::ByteSize(x).to_string())
-                            .unwrap_or_else(|| "n/a".to_string())
-                    ))),
-            )
+                            .unwrap_or_else(|| "n/a".to_string());
+
+            view! {
+                div [
+                    div [
+                        "DB size: "
+                        {db_size}
+                    ]
+                    div [
+                        "Asset size: "
+                        {asset_size}
+                    ]
+                    div [
+                        "Storage size: "
+                        {storage_size}
+                    ]
+                ]
+            }
         });
 
-        box_()
-            .and(subtitle_4().and("Status"))
-            .and(backend)
-            .into_view()
+        view! {
+            div(class="box") [
+                div(class="subtitle is-4") [ "Status" ]
+                {backend}
+            ]
+        }
     })
 }
