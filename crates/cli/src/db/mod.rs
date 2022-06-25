18cb18d4cdd3b227a3ea141e44e3ff40266bb2a3
Christoph Herzog <chris@theduke.at>
2022-06-25 20:36:32 +0200

    refactor(cli): Move compact command to db submodule + rename to log-compact

--- crates/cli/src/db/mod.rs
+++ crates/cli/src/db/mod.rs
@@ -1,9 +1,11 @@
 mod create;
+mod log_compact;
 mod log_recover_data;
 
 #[derive(clap::Subcommand)]
 pub(crate) enum DbCmd {
     LogRecoverData(log_recover_data::LogRecoverDataCmd),
+    LogCompact(log_compact::LogCompactCmd),
     Create(create::CreateCmd),
 }
 
@@ -12,6 +14,9 @@ impl DbCmd {
         match self {
             Self::LogRecoverData(cmd) => {
                 cmd.run();
+            }
+            Self::LogCompact(cmd) => {
+                cmd.run().unwrap();
             }
             Self::Create(cmd) => {
                 cmd.run();
--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,7 +1,7 @@
 mod cmd_upload;
 mod db;
 
-use anyhow::{anyhow, bail, Context};
+use anyhow::{anyhow, bail};
 use factordb::AnyError;
 use semantic_core::{
     api::{self, DbConfig},
@@ -11,7 +11,7 @@ use semantic_core::{
 use std::{io::Write, path::PathBuf, sync::Arc};
 
 use semantic::{
-    app::{self, App, AppConfig},
+    app::{self, AppConfig},
     server,
 };
 
@@ -32,7 +32,6 @@ enum CliCommand {
     #[cfg(feature = "webkit")]
     Webkit(CommandWebkit),
     GenerateTypescript(GenerateTypescript),
-    CompactDb(CommandCompactDb),
     /// Generate an archive that contains all data and blobs.
     Export {
         #[clap(flatten)]
@@ -163,20 +162,6 @@ struct CommandServer {
     /// eg: `0.0.0.0:3000`
     #[clap(long, env = "SEMANTIC_ADDRESS")]
     address: Option<String>,
-}
-
-#[derive(clap::Parser)]
-struct CommandCompactDb {
-    #[clap(flatten)]
-    backend: BackendOptions,
-    /// The new password to use.
-    /// If not set, the old one will be reused.
-    #[clap(long)]
-    new_password: Option<String>,
-    #[clap(long)]
-    force: bool,
-    /// The path for the new, compacted database.
-    new_path: String,
 }
 
 /// Run a semantic UI inside webkit.
@@ -317,9 +302,6 @@ fn main() {
 
             tracing::info!("All paths imported");
         }
-        CliCommand::CompactDb(cmd) => {
-            compact(cmd).expect("Compaction failed");
-        }
         CliCommand::Upload(cmd) => {
             cmd.run();
         }
@@ -327,96 +309,4 @@ fn main() {
             cmd.run();
         }
     }
-}
-
-fn compact(cmd: CommandCompactDb) -> Result<(), anyhow::Error> {
-    // TODO: this should also "compact" the event log of the factordb, if allowed by config.
-
-    let backend_config = cmd
-        .backend
-        .clone()
-        .build_backend_config()
-        .context("Could not build backend config")?;
-
-    let crypto = match backend_config.db {
-        DbConfig::Crypto(c) => c,
-    };
-
-    tracing::debug!("opening old database...");
-    let old_db = App::build_logfs(&crypto).context("Could not open old database...")?;
-    tracing::info!("old database opened");
-
-    let mut new_config = crypto.clone();
-    if let Some(new_pw) = cmd.new_password {
-        new_config.key = new_pw;
-    }
-
-    if PathBuf::from(&cmd.new_path).exists() && !cmd.force {
-        bail!(
-            "New database location {} already exists! Add --force to overwrite",
-            cmd.new_path
-        );
-    }
-
-    new_config.data_path = Some(cmd.new_path.clone());
-
-    tracing::info!("Creating new database at {}", cmd.new_path);
-    let new_db = App::build_logfs(&new_config).context("Could not open new database")?;
-
-    let keys = old_db.paths_offset(0, usize::MAX)?;
-
-    use sha2::Digest;
-    let mut old_hash = sha2::Sha512::new();
-    let mut old_size = 0;
-
-    tracing::info!("Copying {} blobs", keys.len());
-
-    for (index, key) in keys.iter().enumerate() {
-        tracing::debug!(path=%key, "copying blob {}/{}", index+1, keys.len());
-        let old_data = old_db
-            .get(&key)?
-            .ok_or_else(|| anyhow!("Could not read key"))?;
-        old_hash.update(&old_data);
-        old_size += old_data.len();
-
-        new_db.insert(key, old_data)?;
-    }
-
-    std::mem::drop(old_db);
-    std::mem::drop(new_db);
-
-    let old_hash = old_hash.finalize();
-
-    // Sanity check.
-    // Compare hash, size and keys.
-    let new_db = App::build_logfs(&new_config).context("Could not open new database")?;
-
-    let new_keys = new_db.paths_offset(0, usize::MAX)?;
-    if new_keys != keys {
-        bail!("Key mismatch: new database does not have the same keys as the old one");
-    }
-    std::mem::drop(keys);
-
-    let mut new_hash = sha2::Sha512::new();
-    let mut new_size = 0;
-    for key in new_keys.into_iter() {
-        let data = new_db
-            .get(&key)?
-            .ok_or_else(|| anyhow!("Could not get key"))?;
-        new_hash.update(&data);
-        new_size += data.len();
-    }
-
-    let new_hash = new_hash.finalize();
-
-    if new_hash != old_hash {
-        bail!("Copy did not suceed: hash mismatch! expected {old_hash:?}, but new hash is {new_hash:?}");
-    }
-    if old_size != new_size {
-        bail!("Copy did not suceed: size mismatch: expected {old_size}, but new db has size of {new_size}");
-    }
-
-    tracing::info!("database compacted into {}!", cmd.new_path);
-
-    Ok(())
 }
