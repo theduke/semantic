7b43d5553e4f00f35b95bb1c3b95755a8af2698a
Christoph Herzog <chris@theduke.at>
2022-06-26 15:41:08 +0200

    build: More default logging for "xtask watch-server" command

--- crates/xtask/src/main.rs
+++ crates/xtask/src/main.rs
@@ -118,8 +118,8 @@ fn cmd_watch_server(default_backend: bool) -> Result<(), DynError> {
 
     if std::env::var("RUST_LOG").is_err() {
         cmd.env(
-            "RUST_LOG", // "semantic=trace,semantic_core=trace,factordb=info",
-            "trace",
+            "RUST_LOG", 
+            "semantic=trace,semantic_core=trace,factordb=info",
         );
     }
     (&mut cmd).run()?;
