eda49a3cf2bfc0b4ab36ff4b39fcf208255eec61
Christoph Herzog <chris@theduke.at>
2022-07-13 02:00:50 +0200

    feat: Improve archive export + add archvie import
    * Export entities in dependency order so that dependents always come
      after the parents
    * Add archive import functionality
    * Add roundtrip test

--- crates/semantic/src/lib.rs
+++ crates/semantic/src/lib.rs
@@ -7,4 +7,47 @@ pub mod plugin;
 pub mod server;
 mod util;
 
+pub use app::App;
 pub use util::api_client::ApiClient;
+
+#[cfg(test)]
+async fn test_app(name: &str, handle: &tokio::runtime::Handle) -> (std::path::PathBuf, App) {
+    use semantic_core::api::{BackendCryptoConfig, DbConfig};
+
+    let data_dir = std::env::temp_dir()
+        .join("semantic")
+        .join("tests")
+        .join(name);
+    if data_dir.is_dir() {
+        std::fs::remove_dir_all(&data_dir).unwrap();
+    }
+
+    let token_key = App::random_token_key();
+
+    let app_config = app::AppConfig {
+        backend: Some(semantic_core::api::BackendConfig {
+            db: DbConfig::Crypto(BackendCryptoConfig {
+                data_path: Some(data_dir.join("db").to_str().unwrap().to_string()),
+                key: "key".to_string(),
+                key_iterations: Some(1),
+                salt: None,
+                raw: false,
+                offset: None,
+                full_index_write_interval: None,
+            }),
+            idle_timeout: None,
+        }),
+        token_key,
+        deno: Some(crate::app::DenoConfig {
+            data_dir: data_dir.join("deno"),
+            plugin_dir: None,
+        }),
+        tmp_dir: None,
+    };
+
+    let app = App::build(app_config.clone(), handle.clone())
+        .await
+        .unwrap();
+
+    (data_dir, app)
+}
--- crates/semantic/src/util/archive.rs
+++ crates/semantic/src/util/archive.rs
@@ -1,14 +1,14 @@
 //! Functionality for exporting and importing database + blob data
 //! from archive files.
 
-use std::collections::BTreeSet;
+use std::{collections::BTreeSet, io::BufRead, path::Path};
 
-use anyhow::anyhow;
+use anyhow::{anyhow, bail};
 use factordb::{
-    prelude::{AttrId, AttrMapExt, AttributeDescriptor, Expr, Id, Order, Select},
-    query::expr,
+    prelude::{AttrMapExt, Batch, DataMap},
     AnyError,
 };
+use futures::StreamExt;
 use semantic_core::base::AttrBlobUri;
 
 use crate::{app::App, blobstore::DynBlobStore};
@@ -26,38 +26,29 @@ pub async fn build_archive(
     let mut data = Vec::new();
     let mut blob_paths = BTreeSet::<String>::new();
 
-    let mut last_id = Id::nil();
     tracing::debug!("starting database export");
     let mut entity_count = 0;
-    loop {
-        let filter = Expr::binary(AttrId::expr(), expr::BinaryOp::Gt, last_id);
-        let page = db
-            .select(
-                Select::new()
-                    .with_filter(filter)
-                    .with_sort(AttrId::expr(), Order::Asc),
-            )
-            .await?;
-        if let Some(id) = page.items.last().and_then(|item| item.data.get_id()) {
-            last_id = id;
-        } else {
-            break;
+
+    let mut stream = crate::db::EntitiesOrderedStream::new(db.clone(), 1_000);
+
+    while let Some(res) = stream.next().await {
+        let (_id, map) = res?;
+
+        if let Some(path) = map.get_attr::<AttrBlobUri>() {
+            blob_paths.insert(path);
         }
 
-        for item in page.items {
-            if let Some(path) = item.data.get_attr::<AttrBlobUri>() {
-                blob_paths.insert(path);
-            }
+        let serialized = serde_json::to_vec(&map)?;
+        data.extend_from_slice(&serialized);
+        data.push(b'\n');
+        entity_count += 1;
 
-            let serialized = serde_json::to_vec(&item.data)?;
-            data.extend_from_slice(&serialized);
-            data.push(b'\n');
-            entity_count += 1;
+        if entity_count % 100 == 0 {
+            tracing::debug!("exported {entity_count} entities...");
         }
-
-        tracing::trace!(%entity_count, "exported entity page");
     }
 
+    tracing::debug!("loaded all entities!");
     tracing::debug!(blob_count=%blob_paths.len(), "starting blob export");
 
     let encoder = flate2::write::GzEncoder::new(output, flate2::Compression::default());
@@ -97,4 +88,146 @@ pub async fn build_archive(
     tracing::info!("Export complete");
 
     Ok(())
+}
+
+pub async fn import_archive(app: &App, archive_path: &Path) -> Result<(), anyhow::Error> {
+    let db = app.require_db()?;
+    let blob = app.require_blob()?;
+
+    let file = std::fs::File::open(archive_path)?;
+    let gz = flate2::read::GzDecoder::new(file);
+    let mut archive = tar::Archive::new(gz);
+
+    let mut batch = Batch::new();
+
+    let mut total_blob_count: u64 = 0;
+
+    for res in archive.entries()? {
+        let entry = res?;
+        let entry_path = entry.path()?;
+        let path = entry_path.to_str().ok_or_else(|| {
+            anyhow::anyhow!("Invalid archvie: non-utf8 path {}", entry_path.display())
+        })?;
+
+        if path.starts_with("_blobs/") {
+            total_blob_count += 1;
+            let real_path = path.replacen("_blobs/", "", 1);
+
+            if blob.get_meta(&real_path).await?.is_some() {
+                // TODO: check blobs for equality?
+                bail!("Duplicate blob path: {real_path}");
+            }
+        } else if path == "data.json" {
+            tracing::info!("Importing entities...");
+            let reader = std::io::BufReader::new(entry);
+            for line_res in reader.lines() {
+                let line = line_res?;
+                if line.trim().is_empty() {
+                    continue;
+                }
+
+                let data: DataMap = serde_json::from_str(&line)?;
+                let id = data
+                    .get_id()
+                    .ok_or_else(|| anyhow::anyhow!("Invalid entity without id: {data:?}"))?;
+
+                batch = batch.and_create(factordb::query::mutate::Create { id, data });
+            }
+        }
+    }
+
+    db.batch(batch).await?;
+    std::mem::drop(archive);
+
+    let file = std::fs::File::open(archive_path)?;
+    let gz = flate2::read::GzDecoder::new(file);
+    let mut archive = tar::Archive::new(gz);
+    for (index, res) in archive.entries()?.enumerate() {
+        let mut entry = res?;
+        let entry_path = entry.path()?;
+        let path = entry_path.to_str().ok_or_else(|| {
+            anyhow::anyhow!("Invalid archvie: non-utf8 path {}", entry_path.display())
+        })?;
+
+        if path.starts_with("_blobs/") {
+            let real_path = path.replacen("_blobs/", "", 1);
+
+            let mut writer = blob.put_std_writer(&real_path).await?;
+            std::io::copy(&mut entry, &mut writer)?;
+            writer.flush()?;
+        } else {
+        }
+
+        tracing::debug!("imported blob {index}/{total_blob_count}");
+    }
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use factordb::prelude::EntityContainer;
+    use semantic_core::api::FileUploadMetadata;
+
+    use super::*;
+
+    #[test]
+    fn test_archive_export_import_roundtrip() {
+        let rt = tokio::runtime::Runtime::new().unwrap();
+        let handle = rt.handle().clone();
+        rt.block_on(async move {
+            let (data_dir, app) =
+                crate::test_app("archive-export-import-roundtrip-export", &handle).await;
+            let db = app.require_db().unwrap();
+
+            let mut files = Vec::new();
+
+            for x in 0u8..10 {
+                let data = vec![x];
+                let typed = app
+                    .upload_file(
+                        FileUploadMetadata {
+                            filename: Some(format!("x{x}.bin")),
+                            title: Some(format!("x{x}.bin")),
+                            url: None,
+                            collection_id: None,
+                            tag_ids: Vec::new(),
+                        },
+                        data.clone(),
+                    )
+                    .await
+                    .unwrap();
+
+                let id = typed.id();
+
+                let map = db.entity(id).await.unwrap();
+                files.push((map, data));
+            }
+
+            let archive_path = data_dir.join("archive.tar.gz");
+
+            let f = std::fs::File::create(&archive_path).unwrap();
+            build_archive(&app, f).await.unwrap();
+
+            app.close_backend().await.unwrap();
+
+            let (_, app2) =
+                crate::test_app("archive-export-import-roundtrip-import", &handle).await;
+            let db = app2.require_db().unwrap();
+            let blob = app2.require_blob().unwrap();
+
+            import_archive(&app2, &archive_path).await.unwrap();
+
+            for (old_file, old_data) in files {
+                let id = old_file.get_id().unwrap();
+                let blob_path = old_file.get_attr::<AttrBlobUri>().unwrap();
+                let new_file = db.entity(id).await.unwrap();
+
+                assert_eq!(old_file, new_file);
+
+                let new_data = blob.get(&blob_path).await.unwrap().unwrap();
+                assert_eq!(old_data, new_data);
+            }
+        });
+    }
 }
