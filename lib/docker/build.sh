6d6d1bfc640c2b599a75c26cb4b1c6871e279dda
Christoph Herzog <chris@theduke.at>
2022-07-25 21:44:14 +0200

    build: Revert docker build git clone changes again
    Still not working properly...

--- lib/docker/build.sh
+++ lib/docker/build.sh
@@ -88,10 +88,7 @@ function build_semantic() {
   source $HOME/.cargo/env
 
   # Must run twice, for whatever reason...
-  mkdir -p /build
-  cp -r /host/.git /build/.git
-  cd /build
-  chown -R root: .git
+  git clone /host /build
   git reset --hard HEAD
 
   export CARGO_NET_GIT_FETCH_WITH_CLI="true"
