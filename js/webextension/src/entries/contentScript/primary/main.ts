b78e1fc82ad0aef88073ad9d07c5dad38cc1be9c
Christoph Herzog <chris@theduke.at>
2022-08-01 18:01:34 +0200

    fix(webextension): Disable auto-generated content script

--- js/webextension/src/entries/contentScript/primary/main.ts
+++ js/webextension/src/entries/contentScript/primary/main.ts
@@ -1,16 +1,14 @@
-import renderContent from "../renderContent";
-import logo from "~/assets/logo.svg";
-import "./style.css";
+// import "./style.css";
 
-renderContent(
-  import.meta.PLUGIN_WEB_EXT_CHUNK_CSS_PATHS,
-  (appRoot: HTMLElement) => {
-    const logoImageUrl = new URL(logo, import.meta.url).href;
+// renderContent(
+//   import.meta.PLUGIN_WEB_EXT_CHUNK_CSS_PATHS,
+//   (appRoot: HTMLElement) => {
+//     const logoImageUrl = new URL(logo, import.meta.url).href;
 
-    appRoot.innerHTML = `
-    <div class="logo">
-      <img src="${logoImageUrl}" height="50" alt="" />
-    </div>
-  `;
-  }
-);
+//     appRoot.innerHTML = `
+//     <div class="logo">
+//       <img src="${logoImageUrl}" height="50" alt="" />
+//     </div>
+//   `;
+//   }
+// );
