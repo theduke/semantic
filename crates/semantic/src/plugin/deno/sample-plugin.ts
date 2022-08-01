92109032e08a8ae0dd94f6f384898e4097fe6500
Christoph Herzog <chris@theduke.at>
2022-08-01 21:42:46 +0200

    docs: Update sample plugin to be compliant

--- crates/semantic/src/plugin/deno/sample-plugin.ts
+++ crates/semantic/src/plugin/deno/sample-plugin.ts
@@ -1,29 +1,57 @@
 
+const SAMPLE_ID = '550e8400-e29b-11d4-a716-446655440000';
+
 class Plugin {
   static NAME = 'sample';
   schema() {
     return {
       name: Plugin.NAME,
+      import_matchers: [
+        {
+          matcher: {Domains: {domains: ["test.com"]}},
+          support: 'Dedicated',
+        }
+      ],
     };
   }
 
-  import(url: string) {
-
-    if (url === 'http://test.com/abc') {
-      return {
-        plugin: Plugin.NAME,
-        items: [
-          {
-            data: {
-              'factor/type': 'semantic/Image',
-              'factor/title': 'Some Image',
-            },
-          }
-        ],
-      };
-    } else {
+  fetchUrl(job: any) {
+    const url = job.url;
+    if (!(url.startsWith('http://test.com') || url.startsWith('https://test.com'))) {
       return null;
     }
+    return {
+      items: [
+        {
+          data: {
+            'factor/type': 'semantic/Image',
+            'factor/title': 'Some Image ' + url,
+            'semantic/url': url,
+          },
+        }
+      ],
+      load_more_url: null,
+      related_urls: [],
+      related_items: [],
+    }
+  }
+
+  import(url: string) {
+    if (!(url.startsWith('http://test.com') || url.startsWith('https://test.com'))) {
+      return null;
+    }
+
+    return {
+      items: [
+        {
+          data: {
+            'factor/type': 'semantic/Image',
+            'factor/title': 'Some Image ' + url,
+            'semantic/url': url,
+          },
+        }
+      ],
+    };
   }
 }
 
