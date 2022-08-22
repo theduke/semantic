ea224da8bf188a90d68cb8c2cddd5df09e3b1ffd
Christoph Herzog <chris@theduke.at>
2022-08-22 17:27:25 +0200

    build(js-ui): Set up eslint

---
+++ js/ui/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { "project": ["./tsconfig.json"] },
+  plugins: ['@typescript-eslint'],
+  root: true,
+
+  overrides: [
+    {
+      "files": ["*.ts", "*.tsx"],
+      "extends": [
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+        "prettier"
+      ],
+      "parser": "@typescript-eslint/parser",
+      "plugins": ["@typescript-eslint"]
+    }
+  ]
+};
