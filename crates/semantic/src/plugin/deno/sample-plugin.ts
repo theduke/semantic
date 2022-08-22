e0ed8868c6e1350e143804f8c25a4035335d2b4b
Christoph Herzog <chris@theduke.at>
2022-08-23 00:39:40 +0200

    docs: Extend sample js plugin to return more data

--- crates/semantic/src/plugin/deno/sample-plugin.ts
+++ crates/semantic/src/plugin/deno/sample-plugin.ts
@@ -1,5 +1,44 @@
 
 const SAMPLE_ID = '550e8400-e29b-11d4-a716-446655440000';
+const SAMPLE_ID2 = '550e8400-e29b-11d4-a716-446655440001';
+const SAMPLE_ID3 = '550e8400-e29b-11d4-a716-446655440002';
+
+function buildData(url: string) {
+    if (!(url.startsWith('http://test.com') || url.startsWith('https://test.com'))) {
+      return null;
+    }
+    return {
+      items: [
+        {
+          data: {
+            'factor/id': SAMPLE_ID,
+            'factor/type': 'semantic/Image',
+            'factor/title': 'Some Image ' + url,
+            'semantic/url': url,
+          },
+        }
+      ],
+      load_more_url: {url: 'https://test.com/more', label: 'Next Page'},
+      related_urls: [
+        { label: 'Page 1', url: 'https://test.com/page/1'},
+        { label: 'Page 2', url: 'https://test.com/page/2'},
+      ],
+      related_items: [
+        { data: {
+            'factor/id': SAMPLE_ID2,
+            'factor/type': 'semantic/Image',
+            'factor/title': 'Related Image ',
+            'semantic/url': 'https://test.com/related',
+        }},
+        {data: {
+            'factor/id': SAMPLE_ID3,
+            'factor/type': 'semantic/Image',
+            'factor/title': 'Related Image ',
+            'semantic/url': 'https://test.com/related',
+        }},
+      ],
+    }
+}
 
 class Plugin {
   static NAME = 'sample';
@@ -16,44 +55,11 @@ class Plugin {
   }
 
   fetchUrl(job: any) {
-    const url = job.url;
-    if (!(url.startsWith('http://test.com') || url.startsWith('https://test.com'))) {
-      return null;
-    }
-    return {
-      items: [
-        {
-          data: {
-            'factor/id': SAMPLE_ID,
-            'factor/type': 'semantic/Image',
-            'factor/title': 'Some Image ' + url,
-            'semantic/url': url,
-          },
-        }
-      ],
-      load_more_url: null,
-      related_urls: [],
-      related_items: [],
-    }
+    return buildData(job.url);
   }
 
   import(url: string) {
-    if (!(url.startsWith('http://test.com') || url.startsWith('https://test.com'))) {
-      return null;
-    }
-
-    return {
-      items: [
-        {
-          data: {
-            'factor/id': SAMPLE_ID,
-            'factor/type': 'semantic/Image',
-            'factor/title': 'Some Image ' + url,
-            'semantic/url': url,
-          },
-        }
-      ],
-    };
+    return buildData(url);
   }
 }
 
