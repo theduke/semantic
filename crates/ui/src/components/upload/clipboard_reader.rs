9f5f4766f1f637afdb909c8f677fe87f55b964d6
Christoph Herzog <chris@theduke.at>
2023-02-10 00:01:59 +0100

    fix(ui): Disable clipboard reader due to event beingn experimental
    web-sys annoyances...

--- crates/ui/src/components/upload/clipboard_reader.rs
+++ crates/ui/src/components/upload/clipboard_reader.rs
@@ -3,20 +3,23 @@ use semantic_ui_core::components::util::notification_default;
 
 // Clipboard API resources:
 // * https://web.dev/async-clipboard/: has examples and explanations
-pub fn clipboard_reader(on_paste: impl Fn(Vec<web_sys::File>) + 'static) -> TagBuilder {
+pub fn clipboard_reader(_on_paste: impl Fn(Vec<web_sys::File>) + 'static) -> TagBuilder {
     let sub = EventSubscription::subscribe(
         web_sys::window().unwrap().into(),
         brass::dom::Ev::Paste,
-        move |ev: web_sys::ClipboardEvent| {
-            if let Some(files) = ev.clipboard_data().and_then(|d| d.files()) {
-                let items: Vec<web_sys::File> = (0..files.length())
-                    .filter_map(|index| files.item(index))
-                    .collect();
-                if !items.is_empty() {
-                    on_paste(items);
-                }
-            }
-        },
+        move |_ev: web_sys::Event| {},
+        // NOTE: currently disabled due to ClipboardEvent being experimental
+        // (it requires a custom --cfg RUSTFLAG)
+        // move |ev: web_sys::ClipboardEvent| {
+        //     if let Some(files) = ev.clipboard_data().and_then(|d| d.files()) {
+        //         let items: Vec<web_sys::File> = (0..files.length())
+        //             .filter_map(|index| files.item(index))
+        //             .collect();
+        //         if !items.is_empty() {
+        //             on_paste(items);
+        //         }
+        //     }
+        // },
     );
     notification_default()
         .bind(sub)
